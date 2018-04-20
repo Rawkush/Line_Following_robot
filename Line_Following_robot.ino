b1de03ca08e024f2c488eb3e3d897535c0e9b7d9
Ankush950 <ankushrawat950@gmail.com>
2018-04-20 18:08:06 +0530

    variables declared
    varailbe decalared for easy remmembrance of the pin and their function, purpose

--- Line_Following_robot.ino
+++ Line_Following_robot.ino
@@ -1,3 +1,11 @@
+int lMotFwd=12; 
+int rMotFwd=6;
+int lMotRev=13;
+int rMotRev=7; 
+
+int onblack= 100;  // reading for black line from sensor is less than this
+int onWhite= 700; // reading for white line is greater than this
+
 void setup() {
   // put your setup code here, to run once:
 

61e9564698325905b8b4d113f2cbb316690d001a
MauryaD <ankushrawat950@gmail.com>
2018-10-28 18:21:45 +0530

    code reformatted for easy Understanding

--- Line_Following_robot.ino
+++ Line_Following_robot.ino
@@ -1,20 +1,20 @@
-int lMotFwd=12; 
-int rMotFwd=6;
-int lMotRev=13;
-int rMotRev=7; 
+const int lMotFwd=12; 
+const int rMotFwd=6;
+const int lMotRev=13;
+const int rMotRev=7; 
 
-int onblack= 100;  // reading for black line from sensor is less than this
-int onWhite= 700; // reading for white line is greater than this
+const int onblack= 100;  // reading for black line from sensor is less than this
+const int onWhite= 700; // reading for white line is greater than this
 
 void setup() {
 
   Serial.begin(9600);
   
-  // pinMode(A0,INPUT); 
+  // pinMode(A0,INPUT);  
   pinMode(A1,INPUT);  //extreme left sensor
-  pinMode(A2,INPUT);
+  pinMode(A2,INPUT);  //left
   pinMode(A3,INPUT);  // middle sensor
-  pinMode(A4,INPUT);
+  pinMode(A4,INPUT);   // right
   pinMode(A5,INPUT);  // extreme right sensor
     
   pinMode(lMotFwd,OUTPUT); 
@@ -25,6 +25,26 @@ void setup() {
  
 
 }
+
+
+bool isOnBlack(int sensor){
+
+if( sensor>=onblack)
+return true;
+else
+return false;
+}
+
+
+bool isOnWhite(int sensor){
+  if(sensor<=onWhite)
+  return true;
+  else
+  return false;
+}
+
+
+
 
 void moveForward()
 {
@@ -65,36 +85,44 @@ digitalWrite(rMotRev,LOW);
 
 void loop() {
   // put your main code here, to run repeatedly:
+int sensor1=analogRead(A1);//sensor1  Extreme Left
+int sensor2=analogRead(A2);//sensor2 
+int sensor3=analogRead(A3);//sensor3  middle
+int sensor4=analogRead(A4);//sensor4  
+int sensor5=analogRead(A5);//sensor5   Extreme right
+Serial.print("extreme Left: ");
+Serial.print(sensor1);
+Serial.print("\t lower left: ");
+Serial.print(sensor2);
+Serial.print("\t middle: ");
+Serial.print(sensor3);
+Serial.print("\t lowerRight: ");
+Serial.print(sensor4);
+Serial.print("\t Extreme Right: ");
+Serial.println(sensor5);
 
-int sensor1=analogRead(A1);//sensor1
-int sensor2=analogRead(A2);//sensor2
-int sensor3=analogRead(A3);//sensor3
-int sensor4=analogRead(A4);//sensor4
-int sensor5=analogRead(A5);//sensor5
 
-Serial.println(sensor1);
- 
-if((sensor5<onblack)&&(sensor2<onblack||sensor3<onblack||sensor4<onblack))
-{
-
+if( isOnBlack(sensor1)){
   turnLeft();
-}else
-if((sensor1>onWhite && sensor5>onWhite)&&(sensor2<onblack||sensor3<onblack||sensor4<onblack))
-{moveForward();
-}else
-if((sensor5<onblack))
-  {
-    turnLeft();
-   
+}else{
+
+  if(isOnBlack(sensor3)&&isOnWhite(sensor1)){
+    moveForward();
   }else
-if(sensor1<onblack)
-  {
+
+  if( isOnWhite(sensor1)&& isOnWhite(sensor3)&& isOnBlack(sensor5)  ){
+    turnRight();
+  }else
+  
+  if(isOnBlack(sensor2)){
+    turnLeft();
+  }else
+  if(isOnBlack(sensor4)){
     turnRight();
   }
-else
-if( sensor1>onWhite && sensor5>onWhite && sensor2<onWhite && sensor2<onWhite && sensor4<onWhite)
-{
-  turnCircle();  
+
+
+
 }
 
 }
